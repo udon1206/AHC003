528dca0136378771027ba0808f41f589b0f0dfce
jupiro <aposti0225@yahoo.co.jp>
2021-05-25 17:29:14 +0900

    山登り

--- tools/main.cpp
+++ tools/main.cpp
@@ -41,6 +41,26 @@ int chcount[H][W];
 int cwcount[H][W];
 int sh, sw, th, tw;
 bool usedW[H], usedH[W];
+std::vector<std::vector<int>> A;
+std::vector<int> LIST[1800];
+std::vector<int> B, C;
+std::mt19937 engine(0);
+int enc(int h, int w, int dir)
+{
+	return dir * 900 + h * W + w;
+}
+std::tuple<int, int, int> dec(int X)
+{
+	return std::make_tuple(X / 900, (X % 900) / W,  X % W);
+}
+inline int cost(int X)
+{
+	auto [dir, h, w] = std::make_tuple(X / 900, (X % 900) / W,  X % W);
+	if(dir == 0)
+		return cw[h][w];
+	else
+		return ch[h][w];
+}
 inline int cost(int sh, int sw, int th, int tw)
 {
 	if(sh == th)
@@ -123,31 +143,39 @@ void solve_last()
 	std::reverse(res.begin(), res.end());
 	cout << res << endl;
 	ll len; cin >> len;
+	B.emplace_back(len);
 	len /= cnt;
 	curh = sh, curw = sw;
+	std::vector<int> a;
 	for(const auto &c : res)
 	{
 		if(c == 'R')
 		{
 			updatecw(curh, curw, len);
+			a.emplace_back(enc(curh, curw, 0));
 			curw += 1;
 		}
 		if(c == 'L')
 		{
 			updatecw(curh, curw - 1, len);
+			a.emplace_back(enc(curh, curw - 1, 0));
 			curw -= 1;
 		}
 		if(c == 'U')
 		{
 			updatech(curh - 1, curw, len);
+			a.emplace_back(enc(curh - 1, curw, 1));
 			curh -= 1;
 		}
 		if(c == 'D')
 		{
 			updatech(curh, curw, len);
+			a.emplace_back(enc(curh, curw, 1));
 			curh += 1;
 		}
 	}
+	std::sort(a.begin(), a.end());
+	A.emplace_back(a);
 }
 
 void solve_first()
@@ -156,15 +184,18 @@ void solve_first()
 	int cnth = std::abs(sh - th);
 	int cntw = std::abs(sw - tw);
 	std::string res;
+	std::vector<int> a;
 	auto movew = [&](int tw)
 	{
 		while(curw < tw)
 		{
+			a.emplace_back(enc(curh, curw, 0));
 			res += 'R';
 			curw += 1;
 		}
 		while(curw > tw)
 		{
+			a.emplace_back(enc(curh, curw - 1, 0));
 			res += 'L';
 			curw -= 1;
 		}
@@ -173,11 +204,13 @@ void solve_first()
 	{
 		while(curh < th)
 		{
+			a.emplace_back(enc(curh, curw, 1));
 			res += 'D';
 			curh += 1;
 		}
 		while(curh > th)
 		{
+			a.emplace_back(enc(curh - 1, curw, 1));
 			res += 'U';
 			curh -= 1;
 		}
@@ -188,6 +221,7 @@ void solve_first()
 		moveh(th);
 		cout << res << endl;
 		ll len; cin >> len;
+		B.emplace_back(len);
 		len /= (cnth + cntw);
 		for (int i = std::min(sw, tw); i <= std::max(sw, tw); ++i)
 		{
@@ -197,6 +231,8 @@ void solve_first()
 		{
 			updatech(i, tw, len);
 		}
+		std::sort(a.begin(), a.end());
+		A.emplace_back(a);
 	}
 	else
 	{
@@ -224,11 +260,14 @@ void solve_first()
 				movew(tw);
 				cout << res << endl;
 				ll len; cin >> len;
+				B.emplace_back(len);
 				len /= (cnth + cntw);
 				for (int i = 0; i < H - 1; ++i)
 				{
 					updatech(i, w, len);
 				}
+				std::sort(a.begin(), a.end());
+				A.emplace_back(a);
 			}
 		}
 		else
@@ -255,11 +294,14 @@ void solve_first()
 				moveh(th);
 				cout << res << endl;
 				ll len; cin >> len;
+				B.emplace_back(len);
 				len /= (cnth + cntw);
 				for (int i = 0; i < W - 1; ++i)
 				{
 					updatecw(h, i, len);
 				}
+				std::sort(a.begin(), a.end());
+				A.emplace_back(a);
 			}
 		}
 	}
@@ -268,13 +310,61 @@ void solve_first()
 void solve(int kkt)
 {
 	std::tie(sh, sw, th, tw) = input();
-	if(kkt < 100)
+	if(kkt < 200)
 	{
 		solve_first();
 	}
 	else
 	{
 		solve_last();
+	}
+	for (const auto &e : A.back())
+	{
+		LIST[e].emplace_back(kkt);
+	}
+	// if(kkt % 10 == 0)
+	{
+		C.resize(B.size());
+		for (int i = 0; i < (int)A.size(); ++i)
+		{
+			C[i] = 0;
+			for (int j = 0; j < (int)A[i].size(); ++j)
+			{
+				C[i] += cost(A[i][j]);
+			}
+		}
+		for (int jupi = 0; jupi < 1500; ++jupi)
+		{
+			const int row = engine() % A.size();
+			const int col = engine() % A[row].size();
+			int change = (int)(engine() % 100) - 50;
+			const int cval = A[row][col];
+			if(cost(cval) + change < 0)
+			{
+				change = -cost(cval);
+			}
+			int diff = 0;
+			for(const auto &idx : LIST[cval])
+			{
+				const int pc = C[idx];
+				const int nc = C[idx] + change;
+				int pd = std::abs(B[idx] - pc);
+				int nd = std::abs(B[idx] - nc);
+				diff += nd - pd;
+			}
+			if(diff < 0)
+			{
+				auto [dir, h, w] = dec(cval);
+				if(dir == 0)
+				{
+					cw[h][w] += change;
+				}
+				else
+				{
+					ch[h][w] += change;
+				}
+			}
+		}
 	}
 }
 int main()
@@ -290,9 +380,10 @@ int main()
   		ch[i][j] = cw[i][j] = 5000;
   	}
   }
-  int kkt = 1000;
-  //cin >> kkt;
-  while(kkt--)
-    solve(1000 - kkt);
+
+  for (int kkt = 0; kkt < 1000; ++kkt)
+  {
+  	solve(kkt);
+  }
   return 0;
 }
