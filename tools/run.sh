740fe0ed6cadda77ebd1d51ff54ba15856e2e9e6
jupiro <aposti0225@yahoo.co.jp>
2021-05-30 06:09:40 +0900

    修正

--- tools/run.sh
+++ tools/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # コンパイル
-g++ -std=c++17 -O2 -o JK.out main.cpp
+g++ -std=c++17 -O2 -Wall -Wextra -o JK.out main.cpp
+#[st, en] のseed のファイルを処理する
 st=1200
 en=1499
 # インタラクティブ処理の関数
@@ -9,16 +10,15 @@ f1(){
 }
 # ビジュアライザに通して，スコアを標準出力として取得する関数
 f2(){
-  if [ $1 = $st];
-  then
-    cargo run --release --bin vis in/$1.txt out/$1.txt > score.txt
-   else
-    cargo run --release --bin vis in/$1.txt out/$1.txt >> score.txt
-   fi
+   cargo run --release --bin vis in/$1.txt out/$1.txt >> score.txt
 }
 # xargs で関数使うための処理
 export -f f1
 export -f f2
+
+#以前の処理を削除
+rm -f score.txt
+
 # 並列処理
 seq -f '%04g' $st $en | xargs -t -n1 -P8 -I{} bash -c "f1 {}"
 seq -f '%04g' $st $en | xargs -t -n1 -P8 -I{} bash -c "f2 {}"
