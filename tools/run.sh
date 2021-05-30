0a1a36ee15805403aec9b2be756955111a01e7e2
jupiro <aposti0225@yahoo.co.jp>
2021-05-30 19:41:12 +0900

    option つけた

--- tools/run.sh
+++ tools/run.sh
@@ -2,12 +2,12 @@
 
 #tools フォルダ下で実行
 cd `dirname $0`
-
 # コンパイル
 g++ -std=c++17 -O2 -Wall -Wextra -o JK.out main.cpp
-#[st, en] のseed のファイルを処理する
+#[st, en] のseed のファイルを処理する．procsはプロセス数
 st=0
-en=300
+en=100
+procs=8
 # インタラクティブ処理の関数
 f1(){
   cargo run --release --bin tester in/$1.txt ./JK.out > out/$1.txt
@@ -24,8 +24,40 @@ export -f f2
 rm -f score.txt
 rm -rf out
 mkdir out
+
+# option
+
+usage(){
+  cat <<EOM
+使い方：
+  -s : 開始 seed
+  -e : 終了 seed
+  -P : プロセス数
+ただし，開始 seed から終了 seed までの入力ファイルは tools/in 下に置いておいてください．
+EOM
+
+  exit 2
+}
+
+while getopts "s:e:P:" optKey; do
+  case "$optKey" in
+    s)
+      st=${OPTARG}
+      ;;
+    e)
+      en=${OPTARG}
+      ;;
+    P)
+      procs=${OPTARG}
+      ;;
+    '-h' | '--help' | *)
+      usage
+      ;;
+  esac
+done
+
 # 並列処理
-seq -f '%04g' $st $en | xargs -t -n1 -P8 -I{} bash -c "f1 {}"
-seq -f '%04g' $st $en | xargs -t -n1 -P8 -I{} bash -c "f2 {}"
+seq -f '%04g' $st $en | xargs -t -n1 -P$procs -I{} bash -c "f1 {}"
+seq -f '%04g' $st $en | xargs -t -n1 -P$procs -I{} bash -c "f2 {}"
 # score.txt に書き込まれたスコアの計算
 python3 evaluate.py
